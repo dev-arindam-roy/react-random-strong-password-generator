3074537bbc6b43bb49571193f1bcc2f0eb846775
Arindam Roy <arindam.roy.developer@gmail.com>
2025-03-01 19:21:24 +0530

    UP

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import AutoPasswordCreator from './tools/password-creator/AutoPasswordCreator';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AutoPasswordCreator />
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+        className="onex-toast"
+      />
+    </div>
+  );
+}
+
+export default App;
